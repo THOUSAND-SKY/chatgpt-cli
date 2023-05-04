fc86143aa0b12becf839f10f8f71c49f9d1d159c
Ossi Karki <g4gossi@gmail.com>
2023-05-04 02:17:53 +0200

    openai: take configurations via env variables

--- ai/openai.py
+++ ai/openai.py
@@ -4,24 +4,30 @@ import reactivex as rx
 from reactivex import operators as ops
 
 # Set your OpenAI API key here
-api_key = os.environ["OPENAI_API_KEY"]
-openai.api_key = api_key
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+_openai_env = {
+    "max_tokens": int(os.environ.get("CHATGPT_CLI_OPENAI_MAX_TOKENS", "1000")),
+    "model": os.environ.get("CHATGPT_CLI_OPENAI_MODEL", "gpt-3.5-turbo"),
+    "temperature": float(os.environ.get("CHATGPT_CLI_OPENAI_TEMPERATURE", "0.5")),
+    "system_prompt": os.environ.get("CHATGPT_CLI_OPENAI_SYSTEM_PROMPT", "You are ChatGPT, a large language model trained by OpenAI. You answer briefly and to the point.")
+}
 
 
 def _msg(role, content):
     return {"role": role, "content": content}
 
 
-system_context = [_msg(
-    "system", "You are ChatGPT, a large language model trained by OpenAI. You answer briefly and to the point.")]
+_system_context = [_msg(
+    "system", _openai_env["system_prompt"])]
 
 
 def request(query, history):
     response = openai.ChatCompletion.create(
-        messages=system_context + history + [_msg("user", query)],
-        temperature=0.5,
-        model="gpt-3.5-turbo",
-        max_tokens=1500,
+        messages=_system_context + history + [_msg("user", query)],
+        temperature=_openai_env["temperature"],
+        model=_openai_env["model"],
+        max_tokens=_openai_env["max_tokens"],
         stream=True,
     )
 
